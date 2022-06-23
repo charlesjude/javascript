2333a702de3a9b843df93438a7ace7cec02010d6
OGUBUIKE IKECHUKWU JUDE <105298213+charlesjude@users.noreply.github.com>
2022-06-23 18:23:43 +0100

    my code is critical one

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# javascript" 
+"# kindly understand this my code the first displays the output from check through inspect ans the second on displays the output after puting the Datas."
---
+++ calc.js
@@ -0,0 +1,17 @@
+"# kindly understand this my code the first displays the output from check through inspect ans the second on displays the output after puting the Datas."
+
+
+let myName = "Ogubuike ikechukwu jude";
+let myHeight = "6ft 7";
+let myCountry = "Nigeria";
+
+console.log(myName);
+console.log(myHeight);
+console.log(myCountry)
+
+var n, h, c
+n = prompt("Enter your name")
+h = prompt("Enter you height")
+c = prompt("Enter you Country")
+document.write(n + "<br>" + h + "<br>" + c)
+console.log(document.write)
